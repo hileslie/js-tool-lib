69e85d23d2e1e403f057ed399b0b51a5a6aff3b7
chao.zhang <chao.zhang@beibei.com>
2020-08-18 10:31:38 +0800

    m

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development', //production
+    mode: 'production', // development
     entry: {
         app: ['./src/index.js']
     },
     output: {
         path: path.resolve(__dirname, '../lib'),
         filename: 'index.js',
+        library: 'jstool',
+        libraryTarget: 'umd', // commonjs2
+        globalObject: 'typeof self !== \'undefined\' ? self : this',
     },
     module: {},
     plugins: [],
