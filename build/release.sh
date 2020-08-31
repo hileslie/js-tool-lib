2aab7b512127d59cc0dfd5b3170c8cd6fd64af05
chao.zhang <chao.zhang@beibei.com>
2020-08-31 16:36:23 +0800

    Feat release

---
+++ build/release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+VERSION=`npx select-version-cli`
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+  VERSION=$VERSION npm run dist
+  echo "dist $VERSION ..."
+
+  npm version $VERSION --message "[release] $VERSION"
+  
+fi
+# nrm use npm
+
+# npm i
+
+# npm run dist
